399719f297814527128e3a7fd5483f230807268e
AVCADO <devavcado@gmail.com>
2020-09-09 13:28:07 -0500

    Modified build script and also grub config

--- src/build.sh
+++ src/build.sh
@@ -13,6 +13,6 @@ rm myos.bin
 ../i686-elf-cross/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 ../i686-elf-cross/bin/i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 mkdir -p isodir/boot/grub
-cp myos.bin isodir/boot/myos.bin
+cp myos.bin isodir/boot/niux.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o niux.iso isodir
