f725fcf55be842c1334e0252d282ef7697567380
AVCADO <devavcado@gmail.com>
2020-09-10 21:39:49 -0500

    modification to the GDT

--- src/gdt.asm
+++ src/gdt.asm
@@ -1,9 +1,23 @@
-extern pointer
+; Sorry future me
+; you're on your own
+GLOBAL pointer 
+global pointer
+extern GDT ; easy
 GLOBAL loadGDT:
     lgdt [pointer]
     
     ; load all the segment registers here
+    ; you can't directly mov to them so you need to do like
+    ; mov ax, <selector>
+    ; mov ds, ax
+    ; mov es, ax
+    ; ...
+    
+    ; you cannot directly set cs, you instead need to do a far jump
+    ; so do like jmp <selector>:<label>
+    ; for instance
 
-
+    jmp 0x08:part2
+part2:
     cli
     ret
--- src/gdt.c
+++ src/gdt.c
@@ -1,20 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-
-// am i doing this right?
+#include <stdint.h>
 
 extern struct GDT {
-    // items to add
+    uint32_t pointer;
 };
 
-// you may want to write this in asm as you will also have to l oad the segment registers
-// in protected mode, the segment registers still serve a purpose
-// they store "selectors", which is  a fancy term for pointers to your GDT entries
-// so CS = byte offset of corresponding entry in your GDT
-// this is why the scheme was called "segmentation"
-
-// so how would i write the GDT assembly file?
 
 void installGDT(){
     // testing something
