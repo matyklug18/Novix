5aad98b7c9c4d14929c159db4f35315716766750
Carson Black <uhhadd@gmail.com>
2020-09-16 17:29:10 -0400

    Make boot sequence fancier

--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "tools/nfetch.c"
+#include "logo.h"
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -56,26 +57,19 @@ static const size_t VGA_HEIGHT = 25;
 
 size_t terminal_row;
 size_t terminal_column;
-uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
 void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
+			terminal_buffer[index] = vga_entry(' ', VGA_COLOR_LIGHT_GREY);
 		}
 	}
-}
-
-void terminal_setcolor(uint8_t color)
-{
-	terminal_color = color;
 }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
@@ -84,10 +78,10 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
-void terminal_putchar(char c)
+void terminal_putchar(char c, uint8_t color)
 {
 	if (c != '\n') {
-		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		terminal_putentryat(c, color, terminal_column, terminal_row);
 	}
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -100,10 +94,59 @@ void terminal_putchar(char c)
 	}
 }
 
+#define CHECK_COLOR(farbe) if (strcomp(#farbe, colorCode)) color = VGA_COLOR_ ## farbe;
+#define STATUS "$WHITE![$LIGHT_BLUE!--$WHITE!] $LIGHT_GREY!"
+#define GOOD "$WHITE![$LIGHT_GREEN!:)$WHITE!] $LIGHT_GREY!"
+
+bool strcomp(const char* lhs, const char *rhs) {
+	for (; *lhs; lhs++) {
+		if (*lhs != *rhs) {
+			return false;
+		}
+		rhs++;
+	}
+	return true;
+}
+
 void terminal_write(const char* data, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+	uint8_t color = VGA_COLOR_LIGHT_GREY;
+	char colorCode[15];
+
+	for (; *data != '\0'; data++) {
+		if (*data == '$') {
+			int iter = 0;
+			data++;
+			while (true) {
+				if (*data != '!') {
+					colorCode[iter] = *data;
+					iter++;
+					data++;
+				} else {
+					data++;
+					break;
+				}
+			}
+			colorCode[iter+1] = 0;
+			CHECK_COLOR(BLACK)
+			CHECK_COLOR(BLUE)
+			CHECK_COLOR(GREEN)
+			CHECK_COLOR(CYAN)
+			CHECK_COLOR(RED)
+			CHECK_COLOR(MAGENTA)
+			CHECK_COLOR(BROWN)
+			CHECK_COLOR(LIGHT_GREY)
+			CHECK_COLOR(DARK_GREY)
+			CHECK_COLOR(LIGHT_BLUE)
+			CHECK_COLOR(LIGHT_GREEN)
+			CHECK_COLOR(LIGHT_CYAN)
+			CHECK_COLOR(LIGHT_RED)
+			CHECK_COLOR(LIGHT_MAGENTA)
+			CHECK_COLOR(LIGHT_BROWN)
+			CHECK_COLOR(WHITE)
+		}
+		terminal_putchar(*data, color);
+	}
 }
 
 void terminal_writestring(const char* data)
@@ -111,21 +154,23 @@ void terminal_writestring(const char* data)
 	terminal_write(data, strlen(data));
 }
 
+void terminal_println(const char* data)
+{
+	terminal_write(data, strlen(data));
+	terminal_write("\n", 1);
+}
+
 void kernel_main(void) {
 	terminal_initialize();
-	// init();
+	terminal_println(logo);
+	terminal_println("");
+	terminal_println("");
 
-	// First we load the GDT
-	terminal_writestring("Loading GDT...");
+	terminal_println(STATUS "Booting Nova Vita...");
+
+	// First we load the GDT and IDT
 	loadGDT();
-	terminal_writestring("Success!\n");
-
-	// Then we load the IDT.
-	terminal_writestring("Loading IDT...");
 	installIDT();
-	terminal_writestring("Success!\n");
 
-	// once the kernel loads the GDT &
-	// IDT,
-	terminal_writestring("Booting normally...\n")
+	terminal_println(GOOD "Boot process complete.");
 }
