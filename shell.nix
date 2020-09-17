698325b7c19cac9c82fad33dd71ae366918f246c
matyklug18 <klugmathias@gmail.com>
2020-09-17 07:32:46 +0200

    added qemu to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 {
-    pkgs ? import <nixpkgs> {
-        crossSystem = {
-            config = "i686-elf";
-        };
-    }
+  pkgs ? import <nixpkgs> {
+    crossSystem = {
+      config = "i686-elf";
+    };
+  }
 }:
 
 pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages.buildPackages; [ mtools xorriso nasm binutils pkgs.buildPackages.clang grub2 ];
+  nativeBuildInputs = with pkgs.buildPackages.buildPackages; [ mtools xorriso nasm binutils pkgs.buildPackages.clang grub2 qemu ];
 }
